c5aa54249f182f4ee5bbac03d9d5e614764aaafa
leveses <boda.zsombor10@gmail.com>
2020-09-07 11:59:49 +0200

    first line

---
+++ fush roh dah/fush roh dah/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace fush_roh_dah
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fush roh dah");
+            Console.ReadKey();
+        }
+    }
+}
